97a48de07413cd8b81eba5fa45468decd53a0a28
avil13 <13.avil@gmail.com>
2015-07-09 13:51:03 +0300

    h5bp css

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 <html lang="en" ng-app="myApp">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Empty</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <base href="/">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.css">
 
@@ -11,6 +15,9 @@
 
 </head>
 <body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
 
     <header ng-include="'templates/nav.html'"></header>
     <div class="container z-depth-3">
